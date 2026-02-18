ab24a92c2dea53197f1a0bd0b892ff22d812fd3b
danieloo72 <daniel07nieto@gmail.com>
2026-02-18 10:34:49 +0100

    1v

---
+++ Clases/DogAPIExtended/src/main/java/org/example/DogList.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import controllers.DogsController;
+
+import java.util.List;
+
+public class DogList {
+
+    private String nombre;
+    private List<String> img;
+
+    public DogList(String nombre, List<String> img) {
+        this.nombre = nombre;
+        this.img = img;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<String> getImg() {
+        return img;
+    }
+
+    public void setImg(List<String> img) {
+        this.img = img;
+    }
+}
--- Clases/DogAPIExtended/src/main/java/org/example/HttpClientDog.java
+++ Clases/DogAPIExtended/src/main/java/org/example/HttpClientDog.java
@@ -1,36 +1,36 @@
-package org.example;
-
-import com.google.gson.Gson;
-import controllers.DogsController;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.List;
-
-public class HttpClientDog {
-
-    // URL de PokeAPI
-    private final String url = "https://dog.ceo/api/breeds/list/all";
-    private final String url1 = "https://dog.ceo/api/breed/hound/images";
-
-    // Cliente HTTP
-    private final HttpClient client = HttpClient.newHttpClient();
-
-    public List<DogsController> getDogName() throws IOException, InterruptedException {
-
-        // Construcción de la petición GET
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
-
-        // Envío de la petición
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        // Parseo del JSON
-        Gson gson = new Gson();
-        List<DogsController> data = gson.fromJson(response.body(), DogsController.class);
-
-        return data;
-    }
-}
+//package org.example;
+//
+//import com.google.gson.Gson;
+//import controllers.DogsController;
+//
+//import java.io.IOException;
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+//import java.util.List;
+//
+//public class HttpClientDog {
+//
+//    // URL de PokeAPI
+//    private final String url = "https://dog.ceo/api/breeds/list/all";
+//    private final String url1 = "https://dog.ceo/api/breed/hound/images";
+//
+//    // Cliente HTTP
+//    private final HttpClient client = HttpClient.newHttpClient();
+//
+//    public List<DogsController> getDogName() throws IOException, InterruptedException {
+//
+//        // Construcción de la petición GET
+//        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+//
+//        // Envío de la petición
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//
+//        // Parseo del JSON
+//        Gson gson = new Gson();
+//        List<DogsController> data = gson.fromJson(response.body(), DogsController.class);
+//
+//        return data;
+//    }
+//}
